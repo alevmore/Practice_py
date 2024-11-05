7e83f51c121331cf09a4cdc273526060b2670a63
Alev More <64901448+alevmore@users.noreply.github.com>
2024-11-06 00:39:05 +0200

    Update and rename homework_4_1.py to Practice_1/Practice_1.py

--- Practice_1/Practice_1.py
+++ Practice_1/Practice_1.py
@@ -1,16 +1,16 @@
 
 def total_salary(path):
-    try: #перевірка на коректність пошуку файлу
+    try: 
         with open(path, 'w', encoding='utf-8') as file: # відкриття ФАЙЛУ
             file.write ('''Alex Korp,3000
     Nikita Borisenko,2000
-    Sitarama Raju,1000''') # записала до файлу текст для перевірки коду 
+    Sitarama Raju,1000''') 
         with open(path, 'r', encoding='utf-8') as file: 
             data_list =[]
             data = [el.strip().split(',') for el in file.readlines()] 
             for num in data:
-                data_list.append (int (num[1])) #формування списку зарплат
-    except FileNotFoundError:  #вивід у разі відсутності файлу або пошкодження
+                data_list.append (int (num[1])) 
+    except FileNotFoundError: 
         return 'File with salaries is not found'
     total = sum (data_list)    
     average = total/len(data_list)
@@ -18,7 +18,7 @@ def total_salary(path):
 
 
 total, average = total_salary("path/to/salary_file.txt")
-print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+print(f"Total salary amount: {total}, Average salary: {average}")
 
 
 
